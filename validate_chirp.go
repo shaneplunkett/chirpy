656a63173dbdfb7befb9083225692a810753218c
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-03-24 21:23:22 +1100

    Commit

--- validate_chirp.go
+++ validate_chirp.go
@@ -23,7 +23,7 @@ func handlerValidate(w http.ResponseWriter, r *http.Request) {
 
 	const maxChirpLength = 140
 	if len(params.Body) > maxChirpLength {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long...", nil)
 		return
 	}
 
