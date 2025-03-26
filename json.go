0e7d8227f211977fafb0803fa8ce6fe4c8804f3a
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-03-26 18:47:05 +1100

    error handling

--- json.go
+++ json.go
@@ -31,5 +31,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Error(err)
+	}
 }
